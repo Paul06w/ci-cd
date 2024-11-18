860803bb44b6fa0c5a7d5c5e459da06d35d7a003
paulw <paul.wasle@htl-saalfelden.at>
2024-11-18 08:48:11 +0100

    Project initialised

---
+++ 241118_CI_CD_WebApplication/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _241118_CI_CD_WebApplication
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
